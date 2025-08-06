43d0eb7599a7a5e4d79247f303f8cf2e692baa9b
NICOLAS SCHNAIDER <49192387@ort>
2025-08-06 10:29:23 -0300

    first commit

---
+++ Models/Tarea.cs
@@ -0,0 +1,21 @@
+using System.Data.SqlClient;
+using Dapper;
+using Newtonsoft.Json;
+public class Tarea{
+    [JsonProperty]
+    public int idTarea {get; set;}
+    [JsonProperty]
+    public string titulo {get; set;}
+    [JsonProperty]
+    public string descripcion {get; set;}
+    [JsonProperty]
+    public DateTime fecha {get; set;}
+    [JsonProperty]
+    public bool finalizada {get; set;}
+    [JsonProperty]
+    public int idUsuario {get; set;}
+    
+    public Tarea(){
+       
+    }
+    }
---
+++ Models/Usuario.cs
@@ -0,0 +1,23 @@
+using System.Data.SqlClient;
+using Dapper;
+using Newtonsoft.Json;
+public class Usuario{
+    [JsonProperty]
+    public int idUsuario {get; set;}
+    [JsonProperty]
+    public string nombre {get; set;}
+    [JsonProperty]
+    public string apellido {get; set;}
+    [JsonProperty]
+    public string foto {get; set;}
+    [JsonProperty]
+    public string username {get; set;}
+    [JsonProperty]
+    public DateTime ultimoLogin {get; set;}
+    [JsonProperty]
+    public string password {get; set;}
+    
+    public Usuario(){
+       
+    }
+    }
